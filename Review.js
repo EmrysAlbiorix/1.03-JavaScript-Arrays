dc6e24a5099c7e445f227009f8a9ac1d68713795
prodhc <zionywood@gmail.com>
2021-07-11 15:44:21 -0700

    main

---
+++ Review.js
@@ -0,0 +1,17 @@
+//functions
+
+const DoSomething = (placeholder_1, placeholder_2) => {
+    console.log(`Do this stuff with ${placeholder_1} and ${placeholder_2}`)
+}
+DoSomething("super cool int", "really long string")
+
+
+//If, else if, else statements
+
+// const AreTheyTheSame = (var_1, var_2) => {
+//     var_1 === var_2 ? console.log("They are extremely equal")
+//     : var_1 == var_2 ? console.log("They are somewhat equal")
+//     : console.log("They are not equal")
+// }
+// AreTheyTheSame(1, 1)
+
